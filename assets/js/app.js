fdf33edd84b1f3477603dbae7705f5b803cef09b
Kasey Cowley <kchunterdeluxe@gmail.com>
2015-04-05 23:00:43 -0600

    Modifies GarageList component to be searchable.
    * Uses exact matching of content entered into SearchBar component to
    filter the list of vehicles.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,4 @@ var React      = require('React');
 var GarageList = require('./components/GarageList.jsx');
 var data       = require('./mock.json');
 
-React.render(React.createElement(GarageList, data), document.getElementsByClassName('garage-list')[0]);
+React.render(React.createElement(GarageList, data), document.querySelector('.garage-list'));
--- assets/js/components/GarageList.jsx
+++ assets/js/components/GarageList.jsx
@@ -1,38 +1,102 @@
 var React = require('React');
 
-var randomInt = function () {
-  return Math.floor(Math.random() * 9 + 1);
+var randomInt = function (min, max) {
+  return Math.floor(Math.random() * max + min);
 };
 
 /**
  * Main GarageList node, uses Bootstrap's list-group
  * TODO: this.props.vehicles data from api.autio.local
  *
- * @public
  * @author Kasey C.
  */
 var GarageList = React.createClass({
+
+  getInitialState: function () {
+    return {
+      filterText: ''
+    };
+  },
+
+  filterList: function (filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  },
+
   render: function () {
+    var vehicles = this.props.vehicles;
+
+    // If we are trying to filter the list, we filter out the results in the array based on make/model.
+    if (this.state.filterText !== '') {
+
+      vehicles = vehicles.filter(function (vehicle) {
+        var makeName  = vehicle.links.make.name;
+        var modelName = vehicle.links.model.name;
+        var filterText = this.state.filterText;
+
+        return makeName.indexOf(filterText) !== -1 || modelName.indexOf(filterText) !== -1;
+      }, this);
+
+    }
+
+    // Take the resulting vehicles array and turn it into a React component
+    var rows = vehicles.map(function (vehicle) {
+      return <GarageList.Vehicle key={vehicle.id} vehicle={vehicle}/>;
+    });
+
     return (
-      <ul className="list-group">
-        {
-          this.props.vehicles.map(function (vehicle) {
-            return <GarageList.Vehicle key={vehicle.id} vehicle={vehicle}/>
-          })
-        }
-      </ul>
+      <div>
+        <GarageList.SearchBar filterText={this.state.filterText} onUserInput={this.filterList}/>
+        <ul className="list-group">
+          {rows}
+        </ul>
+      </div>
     );
   }
+
+});
+
+/**
+ * Search bar goodness! When typing in the input, React auto updates the list
+ * of vehicles that match that description. First by make, then model.
+ *
+ * @author Kasey C.
+ */
+GarageList.SearchBar = React.createClass({
+
+  handleChange: function () {
+    // onUserInput is exposed to us from the parent component, so we can call it in our own component.
+    this.props.onUserInput(
+      this.refs.filterTextInput.getDOMNode().value
+    );
+  },
+
+  render: function () {
+    return (
+      <form>
+        <div className="form-group">
+          <input type="text"
+                 placeholder="Filter vehicles..."
+                 className="form-control"
+                 value={this.props.filterText}
+                 ref="filterTextInput"
+                 onChange={this.handleChange}/>
+        </div>
+      </form>
+    );
+  }
+
 });
 
 /**
  * Vehicle node for GarageList
  * Displays the data for each vehicle
  *
- * @public
  * @author Kasey C.
  */
 GarageList.Vehicle = React.createClass({
+
   render: function () {
     var vehicle = this.props.vehicle;
     var make    = vehicle.links.make;
@@ -40,7 +104,7 @@ GarageList.Vehicle = React.createClass({
 
     return (
       <a className="list-group-item">
-        <span className="badge progress-bar-danger">{randomInt()}</span>
+        <span className="badge progress-bar-danger">{randomInt(1, 10)}</span>
         <h4 className="list-group-item-heading">
           {vehicle.year} {make.name} {model.name}
         </h4>
@@ -52,6 +116,7 @@ GarageList.Vehicle = React.createClass({
       </a>
     );
   }
+
 });
 
 module.exports = GarageList;
