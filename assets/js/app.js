e9087dd2a0a64a9f91f933a8ddd4da23481491b5
Kasey Cowley <kchunterdeluxe@gmail.com>
2015-05-05 23:30:57 -0600

    Guts the GarageList component to start Flux, adds as root element on index.

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+let React = require('react');
+
+let GarageList = require('./components/GarageList');
+
+React.render(<GarageList />, document.body);
--- assets/js/components/GarageList.js
+++ assets/js/components/GarageList.js
@@ -1,11 +1,5 @@
-var React    = require("react");
-var _        = require("lodash");
-
-var randomInt = function (min, max) {
-  return Math.floor(Math.random() * max + min);
-};
-
-// TODO: Determine if there are any anti-patterns with passing props that have been used and don't need to be used in the child component
+var React    = require('react');
+//var _        = require('lodash');
 
 /**
  * Main GarageList node, uses Bootstrap's list-group
@@ -16,47 +10,10 @@ var GarageList = React.createClass({
   },
 
   getInitialState: function () {
-    return {
-      filterText: "",
-      filteredVehicles: []
-    };
-  },
-
-  filterList: function (event) {
-
-    var filterText       = event.target.value;
-    var filteredVehicles = _.filter(this.props.vehicleModels, function (vehicleModel) {
-      var vehicle   = vehicleModel.attributes;
-      var makeName  = vehicle.links.make.name;
-      var modelName = vehicle.links.model.name;
-
-      return makeName.indexOf(filterText) !== -1 || modelName.indexOf(filterText) !== -1;
-    });
-
-    this.setState({
-      filterText: filterText,
-      filteredVehicles: filteredVehicles
-    });
-
-  },
-
-  getVehiclesToDisplay: function () {
-    var vehicles = this.state.filteredVehicles.length !== 0 ? this.state.filteredVehicles : this.props.vehicleModels;
-
-    return _.map(vehicles, function (vehicle) {
-      return <GarageList.Vehicle key={vehicle.id} vehicle={vehicle.attributes}/>;
-    });
+    return {};
   },
 
   render: function () {
-    return (
-      <div className="col-lg-8">
-        <GarageList.SearchBar filterText={this.state.filterText} onUserInput={this.filterList}/>
-        <ul className="list-group">
-          {this.getVehiclesToDisplay()}
-        </ul>
-      </div>
-    );
   }
 
 });
@@ -67,23 +24,7 @@ var GarageList = React.createClass({
  */
 GarageList.SearchBar = React.createClass({
 
-  propTypes: {
-    filterText: React.PropTypes.string.isRequired,
-    onUserInput: React.PropTypes.func.isRequired
-  },
-
   render: function () {
-    return (
-      <form>
-        <div className="form-group">
-          <input type="text"
-                 placeholder="Filter vehicles..."
-                 className="form-control"
-                 value={this.props.filterText}
-                 onChange={this.props.onUserInput}/>
-        </div>
-      </form>
-    );
   }
 
 });
@@ -95,23 +36,6 @@ GarageList.SearchBar = React.createClass({
 GarageList.Vehicle = React.createClass({
 
   render: function () {
-    var vehicle = this.props.vehicle;
-    var make    = vehicle.links.make;
-    var model   = vehicle.links.model;
-
-    return (
-      <a className="list-group-item" href={"/#vehicles/" + vehicle.id}>
-        <span className="badge progress-bar-danger">{randomInt(1, 10)}</span>
-        <h4 className="list-group-item-heading">
-          {vehicle.year} {make.name} {model.name}
-        </h4>
-
-        <p className="list-group-item-text">
-          <div><strong>Mileage:</strong> {vehicle.mileage}</div>
-          <div><strong>VIN:</strong> {vehicle.vin}</div>
-        </p>
-      </a>
-    );
   }
 
 });
